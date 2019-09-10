99b2f77c447100ef92f13879ff08b6248d49cd06
Emmanuel LONGEAU <emmanuel.longeau@gmail.com>
2019-09-10 21:54:27 +0200

    initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for reminder
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,120 @@
+{-# LANGUAGE BlockArguments             #-}
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE PolyKinds                  #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeOperators              #-}
+
+-- {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Server where
+
+-- import           Prelude.Compat
+-- import           Control.Monad.Except
+-- import           Control.Monad.Reader
+import           Data.Aeson
+
+-- import Control.Monad.IO.Class (liftIO)
+-- import qualified Data.Aeson.Parser
+-- import           Data.Aeson.Types
+-- import           Data.Attoparsec.ByteString
+-- import           Data.ByteString               (ByteString)
+-- import           Data.List
+-- import           Data.Maybe
+-- import           Data.String.Conversions
+import           Data.Time.Calendar
+import           GHC.Generics
+
+-- import           Lucid
+-- import           Network.HTTP.Media            ((//), (/:))
+import           Control.Monad.Reader
+import           Data.Pool
+import           Data.UUID
+import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.FromRow
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+-- import           Servant.Types.SourceT          ( source )
+
+-- TODO split this module
+
+data Remind = Remind { id :: UUID
+  , description           :: String
+  , creationDate          :: Day
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON Remind
+
+type ReminderAPI = "reminds" :> Get '[JSON] [Remind]
+
+type DBPool = Pool Connection
+
+newtype Env = Env {
+    dbPool :: DBPool
+    -- TODO add logging
+  }
+
+newtype App a = App
+  { unApp :: ReaderT Env IO a
+  } deriving (Functor, Applicative, Monad, MonadIO, MonadReader Env)
+
+class Has field env where
+  obtain :: env -> field
+
+instance Has DBPool Env where
+  obtain = dbPool
+
+grab :: forall field env m . (MonadReader env m, Has field env) => m field
+grab = asks $ obtain @field
+
+type WithPool env m = (Has DBPool env, MonadReader env m, MonadIO m)
+withPool :: WithPool env m => (Connection -> IO a) -> m a
+withPool f = do
+  pool <- grab @DBPool
+  liftIO $ withResource pool f
+
+selectM :: (WithPool env m, FromRow a) => Query -> m [a]
+selectM queryStr = withPool $ \conn -> query_ conn queryStr
+
+getReminds :: WithPool env m => m [Remind]
+getReminds = selectM "SELECT * from reminders"
+
+server :: ServerT ReminderAPI App
+server = getReminds
+
+reminderApi :: Proxy ReminderAPI
+reminderApi = Proxy
+
+nt :: Env -> App a -> Handler a
+nt env app = liftIO $ runReaderT (unApp app) env
+
+app :: Env -> Application
+app env = serve reminderApi $ hoistServer reminderApi (nt env) server
+
+instance FromRow Remind where
+  fromRow = Remind <$> field <*> field <*> field
+
+mkDbPool :: IO DBPool
+mkDbPool = do
+  -- TODO use env variable
+  let create = connect defaultConnectInfo { connectHost     = "localhost"
+                                          , connectDatabase = "postgres"
+                                          , connectUser     = "postgres"
+                                          , connectPassword = "password"
+                                          }
+  createPool create close 1 0.5 1
+
+main :: IO ()
+main = do
+  pool <- mkDbPool
+  let env = Env { dbPool = pool }
+  run 8080 (app env)
